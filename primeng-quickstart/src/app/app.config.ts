a96230cac127d9da52410cafe98c80a5afacd5b9
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-07-02 14:43:01 +0300

    update version

--- primeng-quickstart/src/app/app.config.ts
+++ primeng-quickstart/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import Aura from '@primeng/themes/aura';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
